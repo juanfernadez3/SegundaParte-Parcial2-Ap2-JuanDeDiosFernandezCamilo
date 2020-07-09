daa9ab4a00460630c4f12b0973559842ef570a5c
Juan De Dios Fernandez Camilo <juan_fernandez3@ucne.edu.do>
2020-07-09 11:51:20 -0400

    Agregar archivos de proyecto.

---
+++ Modelos/FAQ.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimerProyectoAzure.Modelos
+{
+    class FAQ
+    {
+        public string Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
